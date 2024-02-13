62296d702dc970a086415e3bf1dad8af75d1d2a9
EmilStanchev <emilstan4ev03@gmail.com>
2024-02-13 10:25:00 +0200

    Added logic to send emails from contact form with emailjs

--- src/components/form/ContactForm.jsx
+++ src/components/form/ContactForm.jsx
@@ -1,8 +1,13 @@
 import { Button, Input, Textarea } from "@material-tailwind/react";
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Formik, Form } from "formik";
 import * as Yup from "yup";
+import emailjs from "@emailjs/browser";
+import { useRef, useState } from "react";
 
 const ContactForm = () => {
+  const form = useRef();
+  const [isSend, setIsSend] = useState(false);
+
   const validationSchema = Yup.object().shape({
     firstName: Yup.string().required("First Name is required"),
     lastName: Yup.string().required("Last Name is required"),
@@ -16,108 +21,132 @@ const ContactForm = () => {
     email: "",
     message: "",
   };
-
-  const handleSubmit = (values, { resetForm }) => {
-    // Handle form submission
-    console.log(values);
-    resetForm();
+  const handleSubmit = () => {
+    emailjs
+      .sendForm(
+        import.meta.env.VITE_SERVICE_ID,
+        import.meta.env.VITE_TEMPLATE_ID,
+        form.current,
+        {
+          publicKey: import.meta.env.VITE_USER_ID,
+        }
+      )
+      .then(
+        () => {
+          console.log("SUCCESS!");
+          setIsSend(true);
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+        }
+      );
   };
 
   return (
-    <Formik
-      initialValues={initialValues}
-      validationSchema={validationSchema}
-      onSubmit={handleSubmit}
-    >
-      {({ values, errors, touched, handleChange, handleBlur }) => (
-        <Form>
-          <div className="mb-8 grid gap-4 lg:grid-cols-2">
-            <Input
-              color="gray"
-              size="lg"
-              variant="static"
-              placeholder="First Name"
-              name="firstName"
-              value={values.firstName}
-              onChange={handleChange}
-              onBlur={handleBlur}
-              className="focus:outline-none"
-              containerProps={{
-                className:
-                  "!min-w-full mb-3 md:mb-0 border-b-2 border-gray-300",
-              }}
-            />
-            {errors.firstName && touched.firstName && (
-              <div className="text-red-500">{errors.firstName}</div>
-            )}
-            <Input
-              color="gray"
-              size="lg"
-              variant="static"
-              placeholder="Last Name"
-              name="lastName"
-              value={values.lastName}
-              onChange={handleChange}
-              onBlur={handleBlur}
-              className="focus:outline-none"
-              containerProps={{
-                className: "!min-w-full border-b-2 border-gray-300",
-              }}
-            />
-            {errors.lastName && touched.lastName && (
-              <div className="text-red-500">{errors.lastName}</div>
-            )}
-          </div>
-          <div className="mb-8">
-            <Input
-              color="gray"
-              size="lg"
-              variant="static"
-              placeholder="Email"
-              name="email"
-              value={values.email}
-              onChange={handleChange}
-              onBlur={handleBlur}
-              className="focus:outline-none"
-              containerProps={{
-                className: "!min-w-full mb-8 border-b-2 border-gray-300",
-              }}
-            />
-            {errors.email && touched.email && (
-              <div className="text-red-500">{errors.email}</div>
-            )}
-          </div>
-          <div className="mb-8">
-            <Textarea
-              color="gray"
-              size="lg"
-              placeholder="Your Message"
-              name="message"
-              value={values.message}
-              onChange={handleChange}
-              onBlur={handleBlur}
-              className="border-none focus:border-none focus:outline-none"
-              containerProps={{
-                className: "!min-w-full mb-8 border-b-2 border-b-gray-300",
-              }}
-            />
-            {errors.message && touched.message && (
-              <div className="text-red-500">{errors.message}</div>
-            )}
-          </div>
-          <div className="w-full flex justify-end">
-            <Button
-              type="submit"
-              className="w-full md:w-fit"
-              color="gray"
-              size="md"
-            >
-              Send message
-            </Button>
-          </div>
-        </Form>
+    <>
+      {isSend ? (
+        <div className="flex justify-center items-center ">
+          <h3 className="text-[green] text-xl">
+            Thank you for your message. I will be in touch shortly.
+          </h3>
+        </div>
+      ) : (
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={handleSubmit}
+        >
+          {({ values, errors, touched, handleChange, handleBlur }) => (
+            <Form ref={form}>
+              <div className="mb-8 grid gap-4 lg:grid-cols-2">
+                <Input
+                  color="gray"
+                  size="lg"
+                  variant="static"
+                  placeholder="First Name"
+                  name="firstName"
+                  value={values.firstName}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className="focus:outline-none"
+                  containerProps={{
+                    className:
+                      "!min-w-full mb-3 md:mb-0 border-b-2 border-gray-300",
+                  }}
+                />
+                {errors.firstName && touched.firstName && (
+                  <div className="text-red-500">{errors.firstName}</div>
+                )}
+                <Input
+                  color="gray"
+                  size="lg"
+                  variant="static"
+                  placeholder="Last Name"
+                  name="lastName"
+                  value={values.lastName}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className="focus:outline-none"
+                  containerProps={{
+                    className: "!min-w-full border-b-2 border-gray-300",
+                  }}
+                />
+                {errors.lastName && touched.lastName && (
+                  <div className="text-red-500">{errors.lastName}</div>
+                )}
+              </div>
+              <div className="mb-8">
+                <Input
+                  color="gray"
+                  size="lg"
+                  variant="static"
+                  placeholder="Email"
+                  name="email"
+                  value={values.email}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className="focus:outline-none"
+                  containerProps={{
+                    className: "!min-w-full mb-8 border-b-2 border-gray-300",
+                  }}
+                />
+                {errors.email && touched.email && (
+                  <div className="text-red-500">{errors.email}</div>
+                )}
+              </div>
+              <div className="mb-8">
+                <Textarea
+                  color="gray"
+                  size="lg"
+                  placeholder="Your Message"
+                  name="message"
+                  value={values.message}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className="border-none focus:border-none focus:outline-none"
+                  containerProps={{
+                    className: "!min-w-full mb-8 border-b-2 border-b-gray-300",
+                  }}
+                />
+                {errors.message && touched.message && (
+                  <div className="text-red-500">{errors.message}</div>
+                )}
+              </div>
+              <div className="w-full flex justify-end">
+                <Button
+                  type="submit"
+                  className="w-full md:w-fit"
+                  color="gray"
+                  size="md"
+                >
+                  Send message
+                </Button>
+              </div>
+            </Form>
+          )}
+        </Formik>
       )}
-    </Formik>
+    </>
   );
 };
 
--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -9,7 +9,7 @@ import ContactForm from "../form/ContactForm";
 
 const Contact = () => {
   return (
-    <section className="px-8 py-16">
+    <section id="contact" className="px-8 py-16">
       <div className="container mx-auto mb-20 text-center">
         <Typography variant="h1" color="blue-gray" className="mb-4">
           Contact Us
