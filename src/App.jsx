063f398ebff40ba8f556db3b7ee9a86e60e6a1b9
EmilStanchev <emilstan4ev03@gmail.com>
2024-02-28 22:29:52 +0200

    Change the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,19 @@
-import { useState, useEffect } from "react";
 import Navbar from "./components/navigation/NavBar";
 import Contact from "./components/sections/Contact";
 import Hero from "./components/sections/Hero";
 import Projects from "./components/sections/Projects";
 import Skills from "./components/sections/Skills";
-import Spinner from "./components/UI/Spinner"; // Import your spinner component
 import About from "./components/sections/About";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    if (document.readyState === "complete") {
-      setLoading(false);
-    }
-  }, []);
-
   return (
     <>
-      {loading ? (
-        <Spinner />
-      ) : (
-        <>
-          <Navbar />
-          <Hero />
-          <Skills />
-          <Projects />
-          <About />
-          <Contact />
-        </>
-      )}
+      <Navbar />
+      <Hero />
+      <Skills />
+      <Projects />
+      <About />
+      <Contact />
     </>
   );
 }
